c20b9d3a12c3c7a01772e4f9304d316359de1258
JohanaC <jmcarpioe@unl.edu.ec>
2015-02-27 21:10:37 -0500

    21.10

--- application/controllers/alertamedica.php
+++ application/controllers/alertamedica.php
@@ -11,14 +11,12 @@ class Alertamedica extends CI_Controller {
                 $this->load->model('enfermedad_model');
 		$this->load->database();
 	}
-
-        
+ 
         function notifications(){
            $this->load->model('alertamedica_model'); 
            $res = array(
 			'alerta' => $this->alertamedica_model->get_last()	
 		);
-		
 		$this->load->view('alertamedica', $res);
           
         }
